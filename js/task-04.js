7761b33dc225b565b281b9c80262cbb6ef0c061f
Matvei212 <q841479@gmail.com>
2023-02-23 21:26:38 +0200

    Update task-04.js

---
+++ js/task-04.js
@@ -0,0 +1,16 @@
+
+const decrementButton = document.querySelector('[data-action="decrement"]');
+const incrementButton = document.querySelector('[data-action="increment"]');
+const valueEl = document.querySelector('#value');
+
+let counterValue = 0;
+
+decrementButton.addEventListener('click', () => {
+  counterValue--;
+  valueEl.textContent = counterValue;
+});
+
+incrementButton.addEventListener('click', () => {
+  counterValue++;
+  valueEl.textContent = counterValue;
+});
